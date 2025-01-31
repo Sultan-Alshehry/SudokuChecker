39b9df66e976234bab4bba48fe3ffd84df342f83
Sultan-Alshehry <sultan.shell123@gmail.com>
2025-01-31 14:28:56 +0200

    Update SudokuVerifierTest.java

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -6,11 +6,8 @@ public class SudokuVerifierTest {
 
 //implement tests to test Sudokuverifier with boundary values.  Use templates from Task 1 to derive and document test cases.
 
-	// A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
-// An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 	String c = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 	String i = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
-	String gc = "123456789456789123789123456231564897564897231897231564312645978645978312978312645";
 	SudokuVerifier v = new SudokuVerifier();
 
 	@Test
@@ -30,7 +27,14 @@ public class SudokuVerifierTest {
 	public void testShortInput() {
 		String a = "123456789";
 		int result = v.verify(a);
-		assertEquals("correct string", result, -1);
+		assertEquals("short string", result, -1);
+	}
+
+	@Test
+	public void testOneTooShortInput() {
+		String a = "41736982563215894795872431682543716979158643234691275828964357157329168416487529";
+		int result = v.verify(a);
+		assertEquals("one too short string", result, -1);
 	}
 
 	@Test
@@ -48,10 +52,24 @@ public class SudokuVerifierTest {
 	}
 
 	@Test
-	public void testTooLongInput() {
+	public void testOneTooLongInput() {
 		String a = "4173698256321589479587243168254371697915864323469127582896435715732916841648752931";
 		int result = v.verify(a);
-		assertEquals("long string", result, -1);
+		assertEquals("one too long string", result, -1);
+	}
+
+	@Test
+	public void testVeryLongInput() {
+		String a = "41736982563215894795872431682543716979158" +
+				"64323469127582896435715732916841648752931417369" +
+				"825632158947958724316825437169791586432346912758" +
+				"289643571573291684164875293141736982563215894795" +
+				"87243168254371697915864323469127582896435715732" +
+				"91684164875293141736982563215894795872431682543" +
+				"71697915864323469127582896435715732916841648752" +
+				"93141736982563215894795872431682543716979158643";
+		int result = v.verify(a);
+		assertEquals("very long string", result, -1);
 	}
 
 	@Test
@@ -69,12 +87,25 @@ public class SudokuVerifierTest {
 	}
 
 	@Test
-	public void testLessThanOneInput() {
-		String bleh = "407369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	public void testZeroInput() {
+		String bleh = "017369825632158947958724316825437169791586432346912758289643571573291684164875293";
 		int result = v.verify(bleh);
-		assertEquals("negative input", result, -1);
+		assertEquals("zero input", result, -1);
 	}
 
+	@Test
+	public void testNonNumberInput() {
+		String bleh = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+		int result = v.verify(bleh);
+		assertEquals("non number input", result, 1);
+	}
+
+	@Test
+	public void testAllDuplicates() {
+		String bleh = "111111111111111111111111111111111111111111111111111111111111111111111111111111111";
+		int result = v.verify(bleh);
+		assertEquals("all duplicates", result, -2);
+	}
 
 	@Test
 	public void testDuplicateInSubGrid() {
@@ -101,7 +132,6 @@ public class SudokuVerifierTest {
 	public void testMainMethodRuns() {
 		String[] args = {};
 		SudokuVerifier.main(args);
-
 	}
 
 }
